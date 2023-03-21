ade4077d186d72f2ed264b0c5a33c194fd76ee54
Julio <julio.bermudez619@gmail.com>
2023-03-21 16:06:53 -0700

    updated fetch functions

--- weather.js
+++ weather.js
@@ -1,24 +1,32 @@
 
 
-const key = '';
+const key = 'e558da8b51128d17caf06f9526d53b74';
 let place = 'San Diego';
 
-
-
-async function directGeo(){
-    let response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${place}&appid=${key}`);
-    let city = await response.json();
+async function getCoordinates(){
+    let locationResponse = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${place}&appid=${key}`);
+    let city = await locationResponse.json();
     let lat = city[0].lat;
     let lon = city[0].lon;
-    let coordinates = [lat,lon];
+    const coordinates = [lat,lon];
     return coordinates;
 };
 
-async function hitAPI(array){
-    let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${array[0]}&lon=${array[1]}&appid=${key}`);
-    let answer = await response.json();
-    console.log(answer);
 
+async function filterToday(){
+    let location = await getCoordinates();
+    let response  = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${location[0]}&lon=${location[1]}&appid=${key}&units=imperial`);
+    let data = await response.json();
+    
+    const todayWeatherObject = {
+        temperature:  data.main.temp,
+        city: data.name,
+        high: data.main.temp_max,
+        low: data.main.temp_min,
+        status: data.weather[0].main,
+        statusDescription: data.weather[0].description,
+    };
+    console.log(todayWeatherObject);
 };
 
-hitAPI(directGeo);
+filterToday();
